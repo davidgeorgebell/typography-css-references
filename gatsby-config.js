e7fec799fd0e8689b472430b27e3bc86d42234de
davidgeorgebell <bell.dave93@gmail.com>
2019-12-31 13:46:32 +0800

    Start on header component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Typography Examples`,
+    description: `Showcase of different css fonts.`,
+    author: `@davidgeorgebell`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
